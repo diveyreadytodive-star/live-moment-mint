9a52016af8a73c65328a158ddeef13c3485e82a9
diveyreadytodive-9861 <diveyreadytodive@gmail.com>
2026-07-19 18:22:38 +0900

    fix: resolve @noble/hashes/sha3 webpack conflict from mpl-core

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    // Fix @noble/hashes version conflict from @metaplex-foundation/mpl-core
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@noble/hashes/sha3': require.resolve('@noble/hashes/sha3'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
